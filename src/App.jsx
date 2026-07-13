0070b26c08baf2f764a481fde5a64757ecfe9668
omardaakour <Omardaakour2005@gmail.com>
2026-07-13 20:32:09 +0300

    add default dashboard redirect

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Users from "./pages/User";
 import Dashboard from "./pages/Dashboard";
 import Layout from "./components/layout/Layout";
 import Profile from "./pages/Profile";
+import { Navigate } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/users" element={<Users />} />
